5edb9b4c9e24b3ea1dc37894ef468d0ad6efca5e
BZ <benno@zimmer.bz>
2020-06-18 11:04:09 +0200

    set_icon

--- init.lua
+++ init.lua
@@ -6,6 +6,12 @@ local dpi = theme.xresources.apply_dpi
 
 local module = {}
 
+local function set_icon(c, icon)
+	if not c then return end
+	icon = gears.surface(icon)
+	c.icon = icon and icon._native or nil
+end
+
 local function new(config)
 	local cfg = config or {}
 end
